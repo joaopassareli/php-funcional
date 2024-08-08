bcd9cde8e28c2ef0e3a028d0e48434b5c73a912e
Joao Passareli <joaopassareli@gmail.com>
2024-08-07 21:30:01 -0300

    Teste final, utilizando mônadas, funções compostas, pipelines de funções e mais paradigmas de programação funcional com PHP

---
+++ dados.php
@@ -0,0 +1,46 @@
+<?php
+
+use Alura\Fp\Maybe;
+
+return Maybe::of([
+    [
+        'pais' => 'Brasil',
+        'medalhas' => [
+            'ouro' => 9,
+            'prata' => 4,
+            'bronze' => 3
+        ]
+        ],
+        [
+            'pais' => 'Estados Unidos',
+            'medalhas' => [
+                'ouro' => 8,
+                'prata' => 7,
+                'bronze' => 4
+            ]
+        ],
+        [
+            'pais' => 'Japão',
+            'medalhas' => [
+                'ouro' => 10,
+                'prata' => 5,
+                'bronze' => 2
+            ]
+        ],
+        [
+            'pais' => 'Costa rica',
+            'medalhas' => [
+                'ouro' => 2,
+                'prata' => 1,
+                'bronze' => 0
+            ]
+        ],
+        [
+            'pais' => 'Nova zelândia',
+            'medalhas' => [
+                'ouro' => 1,
+                'prata' => 1,
+                'bronze' => 1
+            ]
+        ]
+]);
